71fd11dc67b32003c2dd4be106f9d62dc3c77207
matieckhardt <matias.eckhardt@gmail.com>
2023-03-10 07:18:03 -0300

    a ver ahora

--- client/src/components/Talleres/Talleres.jsx
+++ client/src/components/Talleres/Talleres.jsx
@@ -20,7 +20,7 @@ import { handleRegister } from "../../services/alumnoCreate";
 import { toast } from "react-hot-toast";
 import { changeRegisteredIn } from "../../redux/actions/globalActions";
 import React from "react";
-import MuiThemeProvider from "@mui/material";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
 function RegisterTaller() {
   const { dispatch } = useContext(AppContext);
