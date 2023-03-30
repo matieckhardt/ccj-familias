8680e2197f7e23810020531c343686628e0dd64e
matieckhardt <matias.eckhardt@gmail.com>
2023-03-30 14:10:14 -0300

    fix model borrado

---
+++ server/models/Talleres.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  nombre: {
+    type: Object,
+    unique: true,
+    required: [true, "El nombre es obligatorio"],
+  },
+  curso: {
+    type: String,
+    required: [true, "El curso es obligatoria"],
+  },
+  taller: {
+    type: String,
+    required: [true, "El nivel es obligatoria"],
+  },
+  instrumento: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("instrumentos", userSchema);
